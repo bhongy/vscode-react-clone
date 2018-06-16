f74d389080338be6c4abea2ca6230162ec493676
Thanik Bhongbhibhat <email@thanik.me>
2018-06-16 08:40:09 -0700

    Refactor main process to make decision on the "entry" to use once - rather than everytime a window is created

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,21 +5,20 @@ import * as url from 'url';
 const isDevelopment = process.env.NODE_ENV !== 'production';
 const windowsById: Map<number, Electron.BrowserWindow | null> = new Map();
 
+const devEntry = `http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`;
+const prodEntry = url.format({
+  protocol: 'file',
+  pathname: path.join(__dirname, '../renderer/index.html'),
+  slashes: true,
+});
+const entry = isDevelopment ? devEntry : prodEntry;
+
 function createWindow() {
   const window = new BrowserWindow();
   const { id } = window;
 
   windowsById.set(id, window);
-
-  const devEntry = `http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`;
-  const prodEntry = url.format({
-    protocol: 'file',
-    pathname: path.join(__dirname, '../renderer/index.html'),
-    slashes: true,
-  });
-
-  window.loadURL(isDevelopment ? devEntry : prodEntry);
-
+  window.loadURL(entry);
   window.webContents.on('devtools-opened', () => {
     window.focus();
   });
